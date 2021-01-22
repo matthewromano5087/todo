e0b9d1bbb27046199846a3a7bc699c4ec78c709a
matthewromano5087 <matthewromano5087@gmail.com>
2021-01-22 17:55:48 -0500

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h1>My React App</h1>
-        <h2>Matthew</h2>
     </div>
   );
 }
