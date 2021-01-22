ee0b5477b0c0c051add0b608b31c7e399ef74d74
matthewromano5087 <matthewromano5087@gmail.com>
2021-01-22 18:26:25 -0500

    Added import and function statements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-        <h1>My React App</h1>
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   );
 }
