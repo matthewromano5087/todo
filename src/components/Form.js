ce115899dbb5999360b101622cf92708f7d3a964
matthewromano5087 <matthewromano5087@gmail.com>
2021-01-23 08:55:37 -0500

    Added Todo.js file

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Form = ({ setInputText, todos, setTodos }) => {
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
     const inputTextHandler = (e) => {
         console.log(e.target.value);
         setInputText(e.target.value);
@@ -8,12 +8,14 @@ const Form = ({ setInputText, todos, setTodos }) => {
     const submitTodoHandler = (e) => {
         e.preventDefault();
         setTodos([
-            ...todos, {text: inputText, completed: false, id: Math.random() * 1000},
+            ...todos, 
+            {text: inputText, completed: false, id: Math.random() * 1000}
         ]);
+        setInputText(""); 
     };
     return (
         <form>
-            <input onChange={inputTextHandler} type="text" class="todo-input"/>
+            <input value={inputText} onChange={inputTextHandler} type="text" class="todo-input"/>
             <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
